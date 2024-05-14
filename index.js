42c3fbd7200532906adfaaa3ad6cbcf7561dd836
Pedro Vinicius <89553913+PedroViniciussd@users.noreply.github.com>
2024-05-14 23:33:19 +0000

    Endpoint 'Reand All [GET] /personagem'

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!!')
+})
+
+const lista = ['Java', 'Kotlin', 'Android']
+
+//Endpoint Read all [GET]/personagens
+
+app.get('/personagem', function (req, res) {
+  res.send(lista) 
 })
 
 app.listen(3000)
